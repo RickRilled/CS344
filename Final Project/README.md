54a50a523d996fa14dbd288556ce9f030125051e
RickRilled <43143779+RickRilled@users.noreply.github.com>
2019-04-23 01:49:51 -0400

    removed duplicate file

--- Final Project/README.md
+++
@@ -1,1 +0,0 @@
-This folder contains the information for my final project.
--- Final Project/translate.py
+++
@@ -1,8 +0,0 @@
-import keras
-import numpy as np
-
-path = keras.utils.get_file(
-    'comedic.txt',
-    origin='D:\RealPyCharm\cs344\Final Project\Scripts\comedic.txt')
-text = open(path).read().lower()
-print('Corpus length:', len(text))
