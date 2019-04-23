68d83debfe1e59b7f33a8e3a6275bf806d66a9c1
RickRilled <43143779+RickRilled@users.noreply.github.com>
2019-04-23 02:50:34 -0400

    update

--- FinalProject/translate.py
+++ FinalProject/translate.py
@@ -3,6 +3,9 @@ import numpy as np
 
 path = keras.utils.get_file(
     'comedic.txt',
-    origin='D:\RealPyCharm\cs344\Final Project\Scripts\comedic.txt')
+    origin='s3://scriptscs344calvincollege/comedic.txt')
 text = open(path).read().lower()
 print('Corpus length:', len(text))
+
+
+
